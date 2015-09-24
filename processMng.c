a3b804f3128badfdfe65167933643a7dc26cd728
Tianxiong Yang <yangtianxiong1991@gmail.com>
2015-09-24 15:02:17 -0400

    StartTimer works good!

--- processMng.c
+++ processMng.c
@@ -1,9 +1,25 @@
 #include "global.h"
 #include "stdio.h"
 #include "syscalls.h"
+#include "queue.h"
 
 void StartTimer(SleepTime){
 	MEMORY_MAPPED_IO mmio;    //for hardware interface
+	struct Process_Control_Block * currentPCB = pcbTable->First_Element->PCB;//get current PCB
+	
+	//get current time
+	long currentTime;
+	mmio.Mode = Z502ReturnValue;
+	mmio.Field1 = mmio.Field2 = mmio.Field3 = 0;
+	MEM_READ(Z502Clock, &mmio);
+	currentTime = mmio.Field1;
+
+//	GET_TIME_OF_DAY(&currentTime);
+
+	//put current PCB into timer Queue
+	printf("currentTime:%d\n", currentTime);
+	long wakeUpTime = SleepTime + currentTime;
+	enTimerQueue(currentPCB, wakeUpTime);
 
 	// Start the timer - here's the sequence to use
 	mmio.Mode = Z502Start;
